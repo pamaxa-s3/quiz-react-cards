695e009640397ae66f77a5062b32c0a0a6f8d085
Roman Strynzha <roman.strynzha@gmail.com>
2025-11-06 09:48:09 +0200

    mainLayout, routing, global css

--- quiz-react-cards/src/App.jsx
+++ quiz-react-cards/src/App.jsx
@@ -1,10 +1,20 @@
-import {MainLayout} from "./components/MainLayout/"
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { MainLayout } from './components/MainLayout/';
+import './App.css';
 
 export default function App() {
-  return (
-    <>
-     <MainLayout />
-    </>
-  )
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route element={<MainLayout />}>
+					<Route path='/' element={<div>Home</div>} />
+					<Route path='/about' element={<div>about</div>} />
+					<Route path='/addquestion' element={<h1>Add question</h1>} />
+					<Route path='/forbiden' element={<h1>Forbiden</h1>} />
+					<Route path='/*' element={<h1>404</h1>} />
+				</Route>
+			</Routes>
+			<MainLayout />
+		</BrowserRouter>
+	);
 }
--- quiz-react-cards/src/components/MainLayout/MainLayout.jsx
+++ quiz-react-cards/src/components/MainLayout/MainLayout.jsx
@@ -1,15 +1,18 @@
-import { Header } from "../../components/Header/";
-import { Footer } from "../../components/Footer/";
-import cls from "./MainLayout.module.css";
+import {Outlet} from 'react-router-dom'
+import { Header } from '../../components/Header/';
+import { Footer } from '../../components/Footer/';
+import cls from './MainLayout.module.css';
 
 export const MainLayout = () => {
-    return (
-        <div className={cls.mainLayout}>
-            <Header />
-	<div className={cls.mainWrapper}>
-            <main className={cls.main}>MainLayout</main>
-            <Footer />
-        </div>
-    </div>
-    );
+	return (
+		<div className={cls.mainLayout}>
+			<Header />
+			<div className={cls.mainWrapper}>
+				<main className={cls.main}>
+					<Outlet />
+				</main>
+				<Footer />
+			</div>
+		</div>
+	);
 };
