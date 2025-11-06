8b91cebd51b1f01c0d0a597f15407280cdb125dd
Roman Strynzha <roman.strynzha@gmail.com>
2025-11-06 13:12:30 +0200

    header and button implementation

--- quiz-react-cards/src/components/Button/Button.jsx
+++ quiz-react-cards/src/components/Button/Button.jsx
@@ -1,7 +1,13 @@
-import cls from "./Button.module.css"
+import cls from './Button.module.css';
 
-export const Button = ({children}) => { 
-  return (
-    <button>{children}</button>
-    )
-}
+export const Button = ({ children, onClick, isPrimary, isActive , isDisabled }) => {
+	return (
+		<button
+			className={`${cls.button} ${isPrimary && cls.primary} ${isActive && cls.active} ${isDisabled && cls.disabled}`}
+			onClick={onClick}
+			disabled={isDisabled}
+		>
+			{children}
+		</button>
+	);
+};
--- quiz-react-cards/src/components/Header/Header.jsx
+++ quiz-react-cards/src/components/Header/Header.jsx
@@ -1,7 +1,19 @@
-import cls from "./Header.module.css"
+import logo from '../../../public/vite.svg';
+import { Button } from '../Button/';
+import cls from './Header.module.css';
 
 export function Header() {
-  return (
-    <header>Header</header>
-    )
+	return (
+		<header className={cls.header}>
+			<p>
+				<img src={logo} alt='React logo' />
+				<span>ReactQuiz</span>
+			</p>
+
+			<div className={cls.headerControlers}>
+				<Button isActive>Add</Button>
+				<Button isDisabled>Login</Button>
+			</div>
+		</header>
+	);
 }
