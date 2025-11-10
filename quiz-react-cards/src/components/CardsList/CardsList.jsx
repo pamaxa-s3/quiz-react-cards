5276ea683b79cd6206f53c9e873ad0c469cad8ea
Roman Strynzha <roman.strynzha@gmail.com>
2025-11-10 15:46:51 +0200

    fetchCardsData, need to fix

--- quiz-react-cards/src/components/CardsList/CardsList.jsx
+++ quiz-react-cards/src/components/CardsList/CardsList.jsx
@@ -1,33 +1,66 @@
-import cls from './CardsList.module.css';
+import { useState, useEffect } from 'react';
+import { API_URL } from '../../constans';
 import { Card } from '../Card/';
+import cls from './CardsList.module.css';
 
-export const CardsList = ({ cards }) => {
-	return (
-		<ul className={cls.cardsList}>
-			{cards.map(
-				({
-					id,
-					question,
-					answer,
-					description,
-					resources,
-					level,
-					completed,
-					editDate
-				}) => (
-					<Card
-		key={id}
-						id={id}
-						title={question}
-						answer={answer}
-						description={description}
-						links={resources}
-						level={level}
-						completed={completed}
-						editDate={editDate}
-					/>
-				)
-			)}
-		</ul>
-	);
+export const CardsList = () => {
+	const [cards, setCards] = useState([]);
+	const [error, setError] = useState(null);
+	const [isLoading, setIsLoading] = useState(false);
+
+	const  fetchData= async()=> {
+		try {
+			setIsLoading(true);
+			const res = await fetch(`${API_URL}/react`);
+			if (!cardsData.ok) throw new Error('Fetch has bad request');
+
+			const cardsData = await res.json();
+			setCards(cardsData);
+			// return cardsData;
+		} catch (error) {
+			setError(error.message);
+		} finally {
+			setIsLoading(false);
+		}
+	}
+
+	useEffect(() => {
+		fetchData();
+	}, []);
+
+	if (isLoading) return <h1>Loading...</h1>;
+
+	if (error) return <h1>{error.message}</h1>;
+
+// 	if (cards.length > 0) {
+		return (
+			<ul className={cls.cardsList}>
+				{cards.length &&
+					cards.map(
+						({
+							id,
+							question,
+							answer,
+							description,
+							resources,
+							level,
+							completed,
+							editDate
+						}) => (
+							<Card
+								key={id}
+								id={id}
+								title={question}
+								answer={answer}
+								description={description}
+								links={resources}
+								level={level}
+								completed={completed}
+								editDate={editDate}
+							/>
+						)
+					)}
+			</ul>
+		);
+// 	}
 };
